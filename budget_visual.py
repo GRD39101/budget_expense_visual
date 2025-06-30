3f94fdcf4153144ce2d4218cb588fedfe95b8d9d
GRD39101 <grobertdeaniii@gmail.com>
2025-06-30 00:17:37 -0400

    add print statement into main

--- budget_visual.py
+++ budget_visual.py
@@ -7,6 +7,7 @@ st.header('Visualize your budget monthly expenses')
 st.subheader('The goal of this project is to visualize your monthly expenses in a pie chart and bar chart.')
 def subscription_name():
     subscription_name = st.text_input('Enter the name of the subscription:')
+
     return subscription_name
 def subscription_cost():
     subscription_cost = st.number_input('Enter the cost of the subscription:', min_value=0.0, format="%.2f")
@@ -14,8 +15,9 @@ def subscription_cost():
 
 
 def main():
-    subscription_name_input = subscription_name()
-    subscription_cost_input = subscription_cost()
+    print(f'{subscription_name} cost ${subscription_cost}')
+    # subscription_name_input = subscription_name()
+    # subscription_cost_input = subscription_cost()
     # Do something with the inputs (e.g., create a chart)
 
 if __name__ == "__main__":
